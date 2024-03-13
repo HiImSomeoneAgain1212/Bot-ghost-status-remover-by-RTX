4c6ee306fe145d41dc5b4bbbbff36ec51f85375b
HiImSomeoneAgain1212 <161776679+HiImSomeoneAgain1212@users.noreply.github.com>
2024-03-12 21:38:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Playing Grand Theft Auto VI"];
 
 
 let currentIndex = 0;
